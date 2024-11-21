f63702357f01cca54f45d6ee0321991ad3141f7b
Ivan Parra <nesivapama@gmail.com>
2024-11-21 11:40:37 -0500

    finished basic node express backend

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
 import linkRouter from "./routes/link.route.js";
 import redirectRouter from "./routes/redirect.route.js";
+import cors from "cors";
 
 const app = express();
 
+const whitelist = [process.env.ALLOWED_ORIGIN];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelist.includes(origin) || !origin) {
+        return callback(null, origin);
+      }
+      return callback("Error: Not allowed by CORS:" + origin);
+    },
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", redirectRouter); // back redirect (optional)
